9e744d6370e3304aa0b237ae1885ed9eb6821d12
SanDeber <debernardisantino05@gmail.com>
2026-04-27 08:57:07 -0300

    Primer commit

---
+++ src/main/java/com/bodypaint/backend/bodypanit/BodypanitApplication.java
@@ -0,0 +1,13 @@
+package com.bodypaint.backend.bodypanit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodypanitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodypanitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bodypaint/backend/bodypanit/BodypanitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodypaint.backend.bodypanit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodypanitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
